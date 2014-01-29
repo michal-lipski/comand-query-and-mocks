b0290cc947f1bd4cc72b6e5edee9a3c05a70a1bb
Michal Lipski <kiwaczki@gmail.com>
2014-01-29 16:15:54 +0100

    comments for tests

--- src/test/java/outgoing_query/FlyingSaucerTest.java
+++ src/test/java/outgoing_query/FlyingSaucerTest.java
@@ -12,6 +12,7 @@ public class FlyingSaucerTest {
 
     @Test
     public void gathers_data_from_scanner() {
+        //we need those two ugly lines to stub scanner
         Scanner scanner = mock(Scanner.class);
         when(scanner.getData()).thenReturn(1.5);
         FlyingSaucer flyingSaucer = new FlyingSaucer(scanner);
@@ -29,7 +30,10 @@ public class FlyingSaucerTest {
 
 
 
-    //BETTER WAY
+    //BETTER WAY - we are independent of Scanner we only use data that we need. Some other tie-together class that will
+    //              have to fetch data and mak it available for DataAwareFlyingSaucer.
+    //              Its job will be to simply execute commands and glue other classes together.
+    //              Maybe we will not need to test it at all?
     @Test
     public void gathers_data_() {
         DataAwareFlyingSaucer flyingSaucer = new DataAwareFlyingSaucer(1.5);
